30b2ddcc8b9e33d0fde31d3cb94324fa52cf9d12
CedricMlg <malingre.cedric@gmail.com>
2023-04-06 19:57:05 +0200

    Delete useless react-query code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import "./css/main.css";
@@ -12,22 +11,18 @@ import Footer from "./components/Footer";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-const queryClient = new QueryClient();
-
 /* Rendering the app. */
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/sign-in" element={<SignIn />} />
-            <Route exact path="/user" element={<User />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/sign-in" element={<SignIn />} />
+          <Route exact path="/user" element={<User />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
